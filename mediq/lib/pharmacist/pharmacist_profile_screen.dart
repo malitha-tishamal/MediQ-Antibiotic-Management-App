9adad9fb5ee9c5b6286efc28608179895b693e74
malitha-tishamal <malithatishamal@gmail.com>
2026-06-30 13:01:56 +0530

    Update pharmacist_profile_screen.dart

--- mediq/lib/pharmacist/pharmacist_profile_screen.dart
+++ mediq/lib/pharmacist/pharmacist_profile_screen.dart
@@ -365,7 +365,7 @@ class _PharmacistProfileScreenState extends State<PharmacistProfileScreen> {
                   isDestructive: true,
                   onTap: _confirmRemoveImage,
                 ),
-             
+            
             ],
           ),
         );
