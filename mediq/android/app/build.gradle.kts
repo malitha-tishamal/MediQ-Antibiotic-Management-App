2b2be66f94c161ebeebbe3074ecb6d93f02c040f
malitha-tishamal <malithatishamal@gmail.com>
2026-06-30 13:06:57 +0530

    Set minSdk to 24 and update defaultConfig
    Update mediq/android/app/build.gradle.kts: remove TODO comments, set applicationId explicitly and change minSdk to 24 (fix). targetSdk, versionCode, and versionName continue to use the Flutter-provided values.

--- mediq/android/app/build.gradle.kts
+++ mediq/android/app/build.gradle.kts
@@ -23,15 +23,14 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mediq"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
+    applicationId = "com.example.mediq"
+
+    minSdk = 24   // <-- FIX
+
+    targetSdk = flutter.targetSdkVersion
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
+}
 
     buildTypes {
         release {
